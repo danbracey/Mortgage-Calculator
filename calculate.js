4a24cacc85d1771f974235c7342035d390ff8111
Dan Bracey <24969761@edgehill.ac.uk>
2024-02-28 10:39:10 +0000

    Calculate 5% of deposit

--- calculate.js
+++ calculate.js
@@ -2,3 +2,9 @@ document.getElementById("calculator").addEventListener("click", function(event){
     //Prevent default handling of the mortgage calculator, so it can be handled client-side
     event.preventDefault()
 });
+
+document.getElementById("5pc_deposit").addEventListener("click", function(){
+    let purchase_price =  document.getElementById("purchase_price").value
+    //Calculate 5% of purchase price and set the value of the deposit input.
+    document.getElementById("deposit").value = 5 * purchase_price / 100
+});
