3c7bf7576eed5e932c30ac0a051945f611556740
Dan Bracey <24969761@edgehill.ac.uk>
2024-02-28 10:04:41 +0000

    Prevent default form execution

---
+++ calculate.js
@@ -0,0 +1,4 @@
+document.getElementById("calculator").addEventListener("click", function(event){
+    //Prevent default handling of the mortgage calculator, so it can be handled client-side
+    event.preventDefault()
+});
