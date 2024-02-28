7f73c0b00f44808545429158398a50651b99a7dc
Dan Bracey <24969761@edgehill.ac.uk>
2024-02-28 12:32:45 +0000

    Fix rounding bug that appeared after the last commit

--- calculate.js
+++ calculate.js
@@ -3,21 +3,24 @@ document.getElementById("calculate").addEventListener("click", function(event){
     event.preventDefault()
 });
 
-document.getElementById("5pc_deposit").addEventListener("click", function(){
+document.getElementById("5pc_deposit").addEventListener("click", function(event){
+    event.preventDefault()
     let purchase_price =  document.getElementById("purchase_price").value
     //Calculate 5% of purchase price and set the value of the deposit input.
-    document.getElementById("deposit").value = 5 * purchase_price / 100
+    document.getElementById("deposit").value = Math.round(5 * purchase_price / 100);
 });
 
-document.getElementById("10pc_deposit").addEventListener("click", function(){
+document.getElementById("10pc_deposit").addEventListener("click", function(event){
+    event.preventDefault()
     let purchase_price =  document.getElementById("purchase_price").value
     //Calculate 10% of purchase price and set the value of the deposit input.
-    document.getElementById("deposit").value = 10 * purchase_price / 100
+    document.getElementById("deposit").value = Math.round(10 * purchase_price / 100);
 });
 
-document.getElementById("15pc_deposit").addEventListener("click", function(){
+document.getElementById("15pc_deposit").addEventListener("click", function(event){
+    event.preventDefault()
     let purchase_price =  document.getElementById("purchase_price").value
     //Calculate 15% of purchase price and set the value of the deposit input.
-    document.getElementById("deposit").value = 15 * purchase_price / 100
+    document.getElementById("deposit").value = Math.round(15 * purchase_price / 100);
 });
 
